fe7e2a0539e47dd131cf289dadf41c5ddee5ee44
yu-zyu <siragaigarasi@gmail.com>
2021-03-15 02:31:48 +0900

    傾きについてUnity上で分かり易く表現できるようにした

---
+++ Assets/Script/Math_and_Physics/Example_1.5_Tilt(m)/Tilt_Unity.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tilt_Unity : MonoBehaviour
+{
+    public Transform P1_transform;
+    public Transform P2_transform;
+    public float[] P1 = new float[2];
+    public float[] P2 = new float[2];
+    public float m;
+    public LineRenderer lineRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        P1[0] = P1_transform.position.x;
+        P1[1] = P1_transform.position.y;
+        P2[0] = P2_transform.position.x;
+        P2[1] = P2_transform.position.y;
+        m = Math_and_PhysicsLib.slopeBetweenPoints(P1, P2);
+        lineRenderer.SetPosition(0, P1_transform.position);
+        lineRenderer.SetPosition(1, P2_transform.position);
+
+        Debug.LogFormat("float:  "+ m);
+        
+    }
+}
