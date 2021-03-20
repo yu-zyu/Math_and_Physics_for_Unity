1d766e19c13ac0637fcbd50e4a5ce6fc91ce0f16
yu-zyu <siragaigarasi@gmail.com>
2021-03-21 00:44:22 +0900

    傾き切片型や標準型の直線をプログラムに落とし込んだ

---
+++ Assets/Script/Math_and_Physics/Example_1.6_Slope_of_a_line/StraightGenarater.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StraightGenarater : MonoBehaviour
+{
+    public float a;
+    public float b;
+    public bool isGenarete;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (isGenarete)
+        {
+            // すべての子オブジェクトを取得して削除
+            foreach (Transform childTransform in gameObject.transform)
+            {
+                GameObject.Destroy(childTransform.gameObject);
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                float x = i;
+                GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                float y = (a * x) + b;
+                cube.transform.position = new Vector3(x, y, 0);
+                cube.transform.parent = gameObject.transform;
+                Debug.Log(y);
+            }
+            isGenarete = false;
+        }
+
+    }
+
+}
---
+++ Assets/Script/Math_and_Physics/Example_1.6_Slope_of_a_line/StraightGenarater_Default.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StraightGenarater_Default : MonoBehaviour
+{
+    public float a;
+    public float b;
+    public float c;
+    public bool isGenarete;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (isGenarete)
+        {
+            // すべての子オブジェクトを取得して削除
+            foreach (Transform childTransform in gameObject.transform)
+            {
+                GameObject.Destroy(childTransform.gameObject);
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                float x = i;
+                float y = i;
+                y = (-(a * x) + c)/b;
+                    GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                    cube.transform.position = new Vector3(x, y, 0);
+                    cube.transform.parent = gameObject.transform;
+                    Debug.Log(y);
+            }
+            isGenarete = false;
+        }
+    }
+}
