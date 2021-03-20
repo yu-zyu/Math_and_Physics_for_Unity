62ccb75f719ba3f4e736a4a1589c38e2c971cc2f
yu-zyu <siragaigarasi@gmail.com>
2021-03-20 15:23:56 +0900

    与えられた直線に垂直な直線の傾きを求める関数の実装

---
+++ Assets/Script/Math_and_Physics/Example_1.6_Slope_of_a_line/perpSlope/PerpSlope.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PerpSlope : MonoBehaviour
+{
+    public float slope;
+    // Start is called before the first frame update
+    void Start()
+    {
+        float m;
+        m = Math_and_PhysicsLib.perpSlope(slope);
+        Debug.LogFormat("与えられ直線に垂直な直線の傾き:  "+ m);
+    }
+
+}
--- Assets/Script/Math_and_Physics/Math_and_PhysicsLib.cs
+++ Assets/Script/Math_and_Physics/Math_and_PhysicsLib.cs
@@ -6,4 +6,10 @@ public class Math_and_PhysicsLib
     //物理・数学ライブラリ
     [DllImport("NativePluginSample")]
     public static extern float slopeBetweenPoints(float[] P1, float[] P2);
+    [DllImport("NativePluginSample")]
+    public static extern float perpSlope(float slope);
+    [DllImport("NativePluginSample")]
+    public static extern bool arePerp(float slope1, float slope2);
+    [DllImport("NativePluginSample")]
+    public static extern float lineIntersect(float[] L1Point, float L1Slope, float[] L2Point, float L2Slope);
 }
