b89e734d05ed359da9f7ca3e87d9a38e216d49a4
yu-zyu <siragaigarasi@gmail.com>
2021-03-21 06:07:22 +0900

    直線の傾きを求める関数をUnity側で応用した

--- Assets/Script/Math_and_Physics/Example_1.6_Slope_of_a_line/perpSlope/PerpSlope.cs
+++ Assets/Script/Math_and_Physics/Example_1.6_Slope_of_a_line/perpSlope/PerpSlope.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 public class PerpSlope : MonoBehaviour
 {
     public float slope;
+    public float m;
     // Start is called before the first frame update
     void Start()
     {
-        float m;
         m = Math_and_PhysicsLib.perpSlope(slope);
         Debug.LogFormat("与えられ直線に垂直な直線の傾き:  "+ m);
     }
---
+++ Assets/Script/Math_and_Physics/Example_1.7_Call _the_slope_of_a _line/DefaultStraight.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefaultStraight : MonoBehaviour
+{
+    public float a;
+    public float b;
+    public float m;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m = -a / b;
+        Debug.Log("直線の傾き: " + m);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
