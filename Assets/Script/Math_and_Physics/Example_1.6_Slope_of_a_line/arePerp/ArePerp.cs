3da3abf54d1e7d419de17e37e762000086c0f5fe
yu-zyu <siragaigarasi@gmail.com>
2021-03-20 16:21:43 +0900

    2つの直線が垂直かどうか調べる関数をUnity側で呼び出した

---
+++ Assets/Script/Math_and_Physics/Example_1.6_Slope_of_a_line/arePerp/ArePerp.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArePerp : MonoBehaviour
+{
+    public float slope1;
+    public float slope2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        bool isVertical;
+        isVertical = Math_and_PhysicsLib.arePerp(slope1, slope2);
+        string verticalResult = isVertical ? "垂直です": "垂直じゃないです";  
+        Debug.LogFormat(verticalResult);
+
+    }
+}
