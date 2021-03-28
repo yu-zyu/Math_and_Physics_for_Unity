d8aa65a33b0a600c05f2d845b6b73f53ac12438a
yu-zyu <siragaigarasi@gmail.com>
2021-03-29 01:20:44 +0900

    3次元空間における直線の定義のスクリプトを実装した

---
+++ Assets/Script/Math_and_Physics/Example_1.10_Definition_of_a_Straight_Line_in_3D_Space/Player3D.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player3D : MonoBehaviour
+{
+    public Vector3 p1;
+    public Vector3 p2;
+    public Vector3 straightLine;
+    // Start is called before the first frame update
+    void Start()
+    {
+        p1 = gameObject.transform.position;
+        straightLine.x = p2.x - p1.x;
+        straightLine.y = p2.y - p1.y;
+        straightLine.z = p2.z - p1.z;
+        GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        cube.transform.position = straightLine;
+    }
+}
