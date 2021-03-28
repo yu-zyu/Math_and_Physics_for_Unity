4dd4c92d262c9ada8c306906e807c3950bbf3051
yu-zyu <siragaigarasi@gmail.com>
2021-03-28 20:58:37 +0900

    例題1.9をUnityで再現

--- Assets/Script/Math_and_Physics/Example_1.6_Slope_of_a_line/StraightGenarater.cs
+++ Assets/Script/Math_and_Physics/Example_1.6_Slope_of_a_line/StraightGenarater.cs
@@ -6,9 +6,12 @@ public class StraightGenarater : MonoBehaviour
 {
     public float a;
     public float b;
+    public float x1;
+    public float y1;
     public bool isGenarete;
     public GameObject straightLine;
     public List<Vector3> childrenPosition;
+    public bool IsVerticalStraightLine = false;
 
     // Update is called once per frame
     void Update()
@@ -17,7 +20,7 @@ public class StraightGenarater : MonoBehaviour
         if (isGenarete)
         {
 
-            if(straightLine == null)
+            if (straightLine == null)
             {
                 straightLine = GameObject.CreatePrimitive(PrimitiveType.Cube);
             }
@@ -25,19 +28,35 @@ public class StraightGenarater : MonoBehaviour
             foreach (Transform childTransform in straightLine.transform)
             {
                 GameObject.Destroy(childTransform.gameObject);
+                childrenPosition.Clear();
             }
 
             for (int i = 0; i < 50; i++)
             {
                 float x = i;
                 GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+
                 float y = (a * x) + b;
+                if (IsVerticalStraightLine)
+                {
+                    float m = Math_and_PhysicsLib.perpSlope(a);
+                    y = m * (x - x1) + y1;
+                }
                 cube.transform.position = new Vector3(x, y, 0);
                 cube.transform.parent = straightLine.transform;
                 childrenPosition.Add(cube.transform.position);
+
+                //今の自分の地点まで直線を生成したら終わり
+                if (x1 == x && y1 == y)
+                {
+                    //配列をリバースして移動しやすいようにする
+                    if (IsVerticalStraightLine)
+                        childrenPosition.Reverse();
+                    isGenarete = false;
+                    return;
+                }
                 Debug.Log(y);
             }
-
             isGenarete = false;
         }
 
--- Assets/Script/Math_and_Physics/Example_1.9_Find_the_equation_of_a_Vertical_Straight_line/Player.cs
+++ Assets/Script/Math_and_Physics/Example_1.9_Find_the_equation_of_a_Vertical_Straight_line/Player.cs
@@ -11,7 +11,7 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -24,22 +24,33 @@ public class Player : MonoBehaviour
 
             isMove = false;
         }
-        
+
     }
 
 
-IEnumerator Move()
+    IEnumerator Move()
     {
-        foreach (Transform childTransform in StraightGenarater.straightLine.transform)
+        foreach (Vector3 childTransform in StraightGenarater.childrenPosition)
         {
-            gameObject.transform.position = childTransform.position;
-            if(gameObject.transform.position == new Vector3(30, 40, 0))
+            gameObject.transform.position = childTransform;
+
+            //この座標に来たら直角を生成
+            if (gameObject.transform.position == new Vector3(30, 40, 0))
             {
-yield break;
-
-
+                if (!StraightGenarater.IsVerticalStraightLine)
+                {
+                    StraightGenarater.x1 = gameObject.transform.position.x;
+                    StraightGenarater.y1 = gameObject.transform.position.y;
+                    StraightGenarater.IsVerticalStraightLine = true;
+                    StraightGenarater.isGenarete = true;
+                    yield break;
+                }
             }
-            yield return new WaitForSeconds(0.5f);
+
+            //今まで移動してきた経路を生成
+            GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            cube.transform.position = gameObject.transform.position;
+            yield return new WaitForSeconds(0.1f);
         }
     }
 }
