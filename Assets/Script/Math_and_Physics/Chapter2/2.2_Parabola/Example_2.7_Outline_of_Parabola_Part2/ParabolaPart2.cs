2528b3b9bdb19f6e8a098a615358cb6f6fbc6237
yu-zyu <siragaigarasi@gmail.com>
2021-04-05 23:44:56 +0900

    放物線の概形その2をUnityで再現した

---
+++ Assets/Script/Math_and_Physics/Chapter2/2.2_Parabola/Example_2.7_Outline_of_Parabola_Part2/ParabolaPart2.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParabolaPart2 : MonoBehaviour
+{
+     public float a;
+    public float h;
+    public float k;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = -100; i < 100; i++)
+        {
+            float y = i;
+            GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            float x = a * ((y - h) * (y - h)) + k;
+            cube.transform.position = new Vector3(x, y, 0);
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
