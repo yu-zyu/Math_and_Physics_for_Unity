97556480c92e85a180bd35d1e2f04745a9d18385
yu-zyu <siragaigarasi@gmail.com>
2021-04-09 02:04:24 +0900

    ２つの円の衝突（最適化）をUnityで再現した

---
+++ Assets/Script/Math_and_Physics/Chapter2/2.4_Application_to_Collision_Detection/Example_Collision_of_Two_Circles/CollisionDetectionOptimisation.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionDetectionOptimisation : MonoBehaviour
+{
+    public CircleGraph player1;
+    public CircleGraph player2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Mathf.Pow(player2.h - player1.h,2) + Mathf.Pow(player2.k - player1.k,2) <= Mathf.Pow(player1.sqrtR + player2.sqrtR, 2))
+        {
+            Debug.Log("衝突しています");
+        }
+        
+    }
+}
