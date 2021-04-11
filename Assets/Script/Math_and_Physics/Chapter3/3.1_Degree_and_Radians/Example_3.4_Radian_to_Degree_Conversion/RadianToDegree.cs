4b1fb21f48b49cbc5e01cd6a3a849963141c8ec6
yu-zyu <siragaigarasi@gmail.com>
2021-04-11 23:16:33 +0900

    ちょっと修正

--- Assets/Script/Math_and_Physics/Chapter3/3.1_Degree_and_Radians/Example_3.4_Radian_to_Degree_Conversion/RadianToDegree.cs
+++ Assets/Script/Math_and_Physics/Chapter3/3.1_Degree_and_Radians/Example_3.4_Radian_to_Degree_Conversion/RadianToDegree.cs
@@ -11,6 +11,7 @@ public class RadianToDegree : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //piが相殺されるので計算がいらない！
         Degree = ((Radian[1]) / Radian[0]) * (180);
         Debug.Log(Degree);
 
