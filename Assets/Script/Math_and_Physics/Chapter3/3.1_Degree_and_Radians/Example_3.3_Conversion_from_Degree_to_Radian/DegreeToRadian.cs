3fb25850d98f38ea80fffb719268bef0c0b4213c
yu-zyu <siragaigarasi@gmail.com>
2021-04-11 23:14:54 +0900

    度とラジアンをUnityで再現した

---
+++ Assets/Script/Math_and_Physics/Chapter3/3.1_Degree_and_Radians/Example_3.3_Conversion_from_Degree_to_Radian/DegreeToRadian.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DegreeToRadian : MonoBehaviour
+{
+
+    public float Degree;
+    public float Radian;
+    float pi = 3.141592654f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Radian = Degree * pi / 180;
+        Debug.Log(Radian);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Script/Math_and_Physics/Chapter3/3.1_Degree_and_Radians/Example_3.4_Radian_to_Degree_Conversion/RadianToDegree.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RadianToDegree : MonoBehaviour
+{
+    public float Degree;
+    public float[] Radian = new float[2];
+    float pi = 3.141592654f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Degree = ((Radian[1]) / Radian[0]) * (180);
+        Debug.Log(Degree);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
