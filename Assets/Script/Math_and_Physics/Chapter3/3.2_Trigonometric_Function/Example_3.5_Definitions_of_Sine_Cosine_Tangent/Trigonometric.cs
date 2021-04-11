5504fd3c3e121b6bab45324135f417aa52411b4e
yu-zyu <siragaigarasi@gmail.com>
2021-04-12 00:08:30 +0900

    サイン、コサイン、タンジェントの定義

---
+++ Assets/Script/Math_and_Physics/Chapter3/3.2_Trigonometric_Function/Example_3.5_Definitions_of_Sine_Cosine_Tangent/Trigonometric.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigonometric : MonoBehaviour
+{
+    public float a;
+    public float b;
+    public float c;
+    public float sinA;
+    public float cosA;
+    public float tanA;
+    public float cscA;
+    public float secA;
+    public float cotA;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        c = Mathf.Sqrt(Mathf.Pow(a, 2) + Mathf.Pow(b, 2));
+        sinA = b / c;
+        cosA = a / c;
+        tanA = b / a;
+
+        cscA = 1 / sinA;
+        secA = 1 / cosA;
+        cotA = 1 / tanA;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
