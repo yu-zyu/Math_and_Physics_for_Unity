1a17aaedb14557b7d7d7df4b39ec3ca4e62eedaa
yu-zyu <siragaigarasi@gmail.com>
2021-04-13 02:24:36 +0900

    コサインの使い方をUnityで再現した

---
+++ Assets/Script/Math_and_Physics/Chapter3/3.2_Trigonometric_Function/Example_3.6_How_to_Use_Cosine/UseCosine.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UseCosine : MonoBehaviour
+{
+    public float hyp;
+    public Distance_Between_Two_Points target;
+    public TrigonometricTable trigonometricTable;
+    public float a;
+    public int rad;
+    // Start is called before the first frame update
+    void Start()
+    {
+     }
+
+    // Update is called once per frame
+    void Update()
+    {
+       hyp = target.distance;
+        rad = Mathf.FloorToInt(gameObject.transform.rotation.eulerAngles.x);
+
+        a = hyp * trigonometricTable.cos_table[rad];
+        Debug.Log("Shadow: "+ a);
+    }
+}
