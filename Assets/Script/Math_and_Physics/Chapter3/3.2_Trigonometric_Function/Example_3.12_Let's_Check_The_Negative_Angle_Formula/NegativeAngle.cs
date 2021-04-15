8f9d0b9efef90336f515ba0bc0a73d943944a5af
yu-zyu <siragaigarasi@gmail.com>
2021-04-16 01:57:24 +0900

    負の角度の公式を確認してみようをUnityで再現した

---
+++ Assets/Script/Math_and_Physics/Chapter3/3.2_Trigonometric_Function/Example_3.12_Let's_Check_The_Negative_Angle_Formula/NegativeAngle.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NegativeAngle : MonoBehaviour
+{
+    public Dictionary<int, float> angleList = new Dictionary<int, float>();
+    public int angle;
+    public float sin;
+    public float minus_sin;
+    // Start is called before the first frame up.PI/6date
+    void Start()
+    {
+        angle = 30;
+        angleList.Add(30, Mathf.PI / 6);
+        angleList.Add(-30, -Mathf.PI / 6);
+        angleList.Add(45, Mathf.PI / 4);
+        angleList.Add(-45, -Mathf.PI / 4);
+        angleList.Add(60, Mathf.PI / 3);
+        angleList.Add(-60, -Mathf.PI / 3);
+        angleList.Add(90, Mathf.PI / 2);
+        angleList.Add(-90, -Mathf.PI / 2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (angleList.ContainsKey(angle))
+        {
+            sin = Mathf.Sin(angleList[angle]);
+            minus_sin = -Mathf.Sin(angleList[angle]);
+        }
+        else
+        {
+            sin = 0;
+            minus_sin = 0;
+        }
+    }
+}
