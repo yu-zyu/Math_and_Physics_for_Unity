90a5ff71307d4110988a6bc4b69c5f7abb1ce7d8
yu-zyu <siragaigarasi@gmail.com>
2021-04-15 09:27:49 +0900

    サインをUnityで再現した

---
+++ Assets/Script/Math_and_Physics/Chapter3/3.2_Trigonometric_Function/Example_3.7_How_to_Use_the_Inverse_Tangent_Function/InverseTangent.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InverseTangent : MonoBehaviour
+{
+
+    public Transform player;
+    public Transform target;
+    public float[] P1 = new float[2];
+    public float[] P2 = new float[2];
+    public float atan;
+    public float atan2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        P1[0] = player.position.x;
+        P1[1] = player.position.y;
+        P2[0] = target.position.x;
+        P2[1] = target.position.y;
+        atan = Math_and_PhysicsLib.calcAngle2D(P1, P2);
+
+         atan2 = (float)Mathf.Atan((P2[1] - P1[1]) / (P2[0] - P1[0])) * 57.29577951f;
+        if(P2[1] < P1[1] && P2[0] > P1[0])
+        {
+        }else if((P2[1] < P1[1] && P2[0] < P2[0]) || (P2[1] > P1[1] && P2[0] < P1[0]))
+        {
+            atan2 = atan2 + 180;
+        }
+        else
+        {
+        //    atan2 = atan2 + 360;
+        }
+
+
+        
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Script/Math_and_Physics/Chapter3/3.2_Trigonometric_Function/Example_3.8_3.11_Sine_Wave/SineWave.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SineWave : MonoBehaviour
+{
+    public float a;
+    public float b;
+    public Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (float x = 0; x < 100; x += 0.1f)
+        {
+            GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            cube.transform.localScale = new Vector3(0.1f ,0.1f, 0.1f);
+            cube.transform.position = new Vector3(x ,b * Mathf.Sin(a * x), 0);
+            cube.transform.parent = gameObject.transform;
+        }
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach(Transform child in gameObject.transform)
+        {
+            float x = child.transform.position.x;
+            float z = child.transform.position.z;
+            child.transform.position = new Vector3(x ,b * Mathf.Sin(a * x), z);
+        }
+
+
+        
+    }
+}
