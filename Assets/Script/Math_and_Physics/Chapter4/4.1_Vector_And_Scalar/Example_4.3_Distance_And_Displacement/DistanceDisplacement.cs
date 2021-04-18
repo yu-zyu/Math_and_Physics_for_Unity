5580ee0b79b6f9c51d7cd550d749340e7fe3d38f
yu-zyu <siragaigarasi@gmail.com>
2021-04-19 00:13:49 +0900

    距離と変位をUnityで再現した

---
+++ Assets/Script/Math_and_Physics/Chapter4/4.1_Vector_And_Scalar/Example_4.3_Distance_And_Displacement/DistanceDisplacement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DistanceDisplacement : MonoBehaviour
+{
+    public Transform player;
+    public float start;
+    public float distance;
+    public float displacement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        start = player.position.x;
+        StartCoroutine(Move(2.0f, 250));
+        StartCoroutine(Move(4.0f, 100));
+        StartCoroutine(Move(6.0f, 450));
+
+        displacement = start - player.position.y;
+    }
+
+
+    IEnumerator Move(float delay, float distanceNum )
+    {
+        //delay秒待つ
+        yield return new WaitForSeconds(delay);
+        distance += Mathf.Abs(player.position.x - distanceNum);
+        player.position = new Vector3(distanceNum, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+}
