92b1b084432c81683d3239bad02c782ecb4bf5a0
yu-zyu <siragaigarasi@gmail.com>
2021-04-18 23:22:52 +0900

    ポンゲーム正の変位をUnityで再現した

---
+++ Assets/Script/Math_and_Physics/Chapter4/4.1_Vector_And_Scalar/Example_4.1_Pong_Game_Positive_Mutation/PongGame.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PongGame : MonoBehaviour
+{
+    public Transform paddle;
+    public float start;
+    public float displacement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        start = paddle.position.y;
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        displacement = start - paddle.position.y;
+        
+    }
+}
