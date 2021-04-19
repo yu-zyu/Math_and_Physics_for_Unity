1bf9a251845586191f82ad41397c74611b051eff
yu-zyu <siragaigarasi@gmail.com>
2021-04-19 13:16:55 +0900

    残り

---
+++ Assets/Script/Math_and_Physics/Chapter4/4.2_Polar_And_Cartesian_Coordinates/Example_4.4_Conversion_from_Polar_Coordinate_Display_to_Cartesian_Coordinate_Display/PolarCoordinateDisplayToCartesianCoordinateDisplay.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PolarCoordinateDisplayToCartesianCoordinateDisplay : MonoBehaviour
+{
+
+    public float mag;
+    public float dir;
+    public float x;
+    public float y;
+
+    public Math_and_PhysicsLib.Vector2D_polar vec = new Math_and_PhysicsLib.Vector2D_polar();
+    public Math_and_PhysicsLib.Vector2D_comp comp = new Math_and_PhysicsLib.Vector2D_comp();
+    // Start is called before the first frame update
+    void Start()
+    {
+        vec.mag = mag;
+        vec.dir = dir;
+        Math_and_PhysicsLib.PolarToCompConversion(vec, ref comp);
+        x = comp.x;
+        y = comp.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Script/Math_and_Physics/Chapter4/4.2_Polar_And_Cartesian_Coordinates/Example_4.5_Conversion_from_Cartesian_Coordinate_Display_to_Polar_Coordinate_Display/CartesianCoordinateToDisplayPolarCoordinateDisplay.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CartesianCoordinateToDisplayPolarCoordinateDisplay : MonoBehaviour
+{
+
+    public float x;
+    public float y;
+    public float mag;
+    public float dir;
+
+    public Math_and_PhysicsLib.Vector2D_polar vec = new Math_and_PhysicsLib.Vector2D_polar();
+    public Math_and_PhysicsLib.Vector2D_comp comp = new Math_and_PhysicsLib.Vector2D_comp();
+    // Start is called before the first frame update
+    void Start()
+    {
+        comp.x = x;
+        comp.y = y;
+        Math_and_PhysicsLib.CompToPolarConversion(comp, ref vec);
+        mag = vec.mag;
+        dir = vec.dir;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
