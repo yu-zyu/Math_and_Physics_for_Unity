646cdab75f43876f0a347b19ff625c0abf55f0d8
yu-zyu <siragaigarasi@gmail.com>
2021-04-21 03:14:11 +0900

    ベクトルの正規化をUnityで再現した

---
+++ Assets/Script/Math_and_Physics/Chapter4/4.4_Vector_Scalar_Times/Example_4.12_Vector_Normalization/VectorNormalization.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VectorNormalization : MonoBehaviour
+{
+    public Vector3 a;
+    public float size;
+    public Vector3 result;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        size = Mathf.Sqrt(Mathf.Pow(a.x, 2) + Mathf.Pow(a.y, 2) + Mathf.Pow(a.z, 2));
+        result = new Vector3(a.x / size, a.y / size, a.z / size);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
