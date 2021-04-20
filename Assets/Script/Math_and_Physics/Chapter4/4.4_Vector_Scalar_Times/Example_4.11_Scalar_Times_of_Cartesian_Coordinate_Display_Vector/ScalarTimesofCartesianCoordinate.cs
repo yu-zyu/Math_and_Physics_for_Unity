f077484fdbf0daf6dfd4b28b37f6ca73cdd8f64d
yu-zyu <siragaigarasi@gmail.com>
2021-04-21 02:17:33 +0900

    デカルト座標表示ベクトルのスカラー倍をUnityで再現した

---
+++ Assets/Script/Math_and_Physics/Chapter4/4.4_Vector_Scalar_Times/Example_4.11_Scalar_Times_of_Cartesian_Coordinate_Display_Vector/ScalarTimesofCartesianCoordinate.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScalarTimesofCartesianCoordinate : MonoBehaviour
+{
+    public float a1;
+    public float a2;
+    public float a;
+    public float a1_result;
+    public float a2_result;
+    // Start is called before the first frame update
+    void Start()
+    {
+        a1_result = a1 * a;
+        a2_result = a2 * a;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
