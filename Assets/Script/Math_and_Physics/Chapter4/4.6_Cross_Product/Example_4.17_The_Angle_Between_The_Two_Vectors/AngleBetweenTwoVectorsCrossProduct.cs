bb27b4e0c76565a4b3ade7e1c59cfdc4e111134f
yu-zyu <siragaigarasi@gmail.com>
2021-04-25 02:08:44 +0900

    2つのベクトルのなす角をUnityで再現した(3次元)

---
+++ Assets/Script/Math_and_Physics/Chapter4/4.6_Cross_Product/Example_4.17_The_Angle_Between_The_Two_Vectors/AngleBetweenTwoVectorsCrossProduct.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AngleBetweenTwoVectorsCrossProduct : MonoBehaviour
+{
+    public GameObject a;
+    public GameObject b;
+    public GameObject c;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        float x = (a.transform.position.y * b.transform.position.z) - (a.transform.position.z * b.transform.position.y);
+        float y = (a.transform.position.z * b.transform.position.x) - (a.transform.position.x * b.transform.position.z);
+        float z = (a.transform.position.x * b.transform.position.y) - (a.transform.position.y * b.transform.position.x);
+        
+        float scale = Mathf.Sqrt(Mathf.Pow(x, 2) + Mathf.Pow(y, 2) + Mathf.Pow(z, 2));
+        Debug.Log(scale);
+        Vector3 normalization = new Vector3(x / scale, y / scale, z / scale);
+        Debug.Log(normalization);
+
+        float scaleA = Mathf.Sqrt(Mathf.Pow(a.transform.position.x, 2) + Mathf.Pow(a.transform.position.y, 2) + Mathf.Pow(a.transform.position.z, 2));
+        float scaleB = Mathf.Sqrt(Mathf.Pow(b.transform.position.x, 2) + Mathf.Pow(b.transform.position.y, 2) + Mathf.Pow(b.transform.position.z, 2));
+
+        float sin = scale / (scaleA * scaleB);
+        Debug.Log("sin " + sin);
+        float Asin = Mathf.Asin(sin);
+        Debug.Log("Acos " + Asin);
+        float deg = Asin * Mathf.Rad2Deg;
+        Debug.Log("deg " + deg);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
