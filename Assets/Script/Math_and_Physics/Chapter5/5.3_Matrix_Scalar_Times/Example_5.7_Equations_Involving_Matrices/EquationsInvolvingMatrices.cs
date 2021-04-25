58f069648320a612edf30b3adcc4206686b7fcc7
yu-zyu <siragaigarasi@gmail.com>
2021-04-26 03:03:43 +0900

    行列を含む方程式をUnityで再現した

---
+++ Assets/Script/Math_and_Physics/Chapter5/5.3_Matrix_Scalar_Times/Example_5.7_Equations_Involving_Matrices/EquationsInvolvingMatrices.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EquationsInvolvingMatrices : MonoBehaviour
+{
+    public float[,] A = new float[,] {
+        { 4, 5 },
+        { 1, -2 }
+    };
+
+    public float[,] B = new float[,] {
+        { 0, -1 },
+        {  -3, 2 }
+    };
+
+    public float[,] X = new float[,] {
+        { 0, 0 },
+        { 0, 0 }
+    };
+
+    public float aA;
+    public float xX;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 2; i++)
+        {
+            for (int j = 0; j < 2; j++)
+            {
+                X[i, j] = xX * (aA * A[i, j] - B[i, j]);
+                Debug.Log(X[i, j]);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
