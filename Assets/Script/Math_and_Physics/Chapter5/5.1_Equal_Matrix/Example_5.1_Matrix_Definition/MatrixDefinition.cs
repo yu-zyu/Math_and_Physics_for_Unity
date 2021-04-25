8a0eb3fb29e31c33be80b416ebb7ac71025f76c1
yu-zyu <siragaigarasi@gmail.com>
2021-04-25 14:49:30 +0900

    行列の定義をUnityで再現した

---
+++ Assets/Script/Math_and_Physics/Chapter5/5.1_Equal_Matrix/Example_5.1_Matrix_Definition/MatrixDefinition.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MatrixDefinition : MonoBehaviour
+{
+    //float[,] matrix = new float[2, 3];
+    public float[,] matrix = new float[,] {
+        { -1, 4, -3 },
+        { 5, 0, -2 }
+    };
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
