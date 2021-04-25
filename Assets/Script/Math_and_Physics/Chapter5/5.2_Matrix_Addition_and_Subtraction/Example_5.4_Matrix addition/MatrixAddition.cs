dfea4c26cf4ad08da3cfba9b105cc7525e8c3872
yu-zyu <siragaigarasi@gmail.com>
2021-04-26 01:43:30 +0900

    行列の足し算をUnityで再現した

---
+++ Assets/Script/Math_and_Physics/Chapter5/5.2_Matrix_Addition_and_Subtraction/Example_5.4_Matrix addition/MatrixAddition.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MatrixAddition : MonoBehaviour
+{
+    public float[,] A = new float[,] {
+        { 1, 4, 7 },
+        { 2, 5, 8 },
+        { 3, 6, 9 }
+    };
+
+    public float[,] B = new float[,] {
+        { -3, 9, 4 },
+        { 0, 1, -1 },
+        { 5, -2, 0 }
+    };
+
+    public float[,] AB = new float[,] {
+        { 0, 0, 0 },
+        { 0, 0, 0 },
+        { 0, 0, 0 }
+    };
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i=0; i < 3; i++)
+        {
+            for(int j=0; j<3; j++)
+            {
+                AB[i, j] = A[i, j] + B[i, j];
+                Debug.Log(AB[i, j]);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
