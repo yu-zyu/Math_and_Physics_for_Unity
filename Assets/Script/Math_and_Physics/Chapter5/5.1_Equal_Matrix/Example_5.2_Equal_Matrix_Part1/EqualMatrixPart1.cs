82e603dc3e3337149c787f690da0c2f266bedf19
yu-zyu <siragaigarasi@gmail.com>
2021-04-25 15:08:23 +0900

    等しい行列　その１

---
+++ Assets/Script/Math_and_Physics/Chapter5/5.1_Equal_Matrix/Example_5.2_Equal_Matrix_Part1/EqualMatrixPart1.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EqualMatrixPart1 : MonoBehaviour
+{
+    public float[,] C = new float[,] {
+        { 1, 2, 3 },
+        { 4, 5, 6 }
+    };
+
+    public float[,] D = new float[,] {
+        { 1, 2, 3 },
+        { 4, 5, 6 },
+        { 7, 8, 9 }
+    };
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("型が違うので等しくない");
+        
+    }
+}
