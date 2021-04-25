2bcffc9255fc5e171dfb54a167e5910fbabc32ae
yu-zyu <siragaigarasi@gmail.com>
2021-04-26 05:14:07 +0900

    型の異なる行列の掛け算を定義できるかUnityで再現した

---
+++ Assets/Script/Math_and_Physics/Chapter5/5.4_Matrix_Multiplication/Example_5.9_Multiplication_of_Matrices_of_Different_Types/MultiplicationOfMatricesOfDifferentTypes.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultiplicationOfMatricesOfDifferentTypes : MonoBehaviour
+{
+    public float[,] B = new float[,] {
+        { 1, 2, 3 }
+    };
+
+    public float[,] A = new float[,] {
+        { 4, 3, -2 },
+        {  -1, 0, 5 }
+    };
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(B.GetLength(0) == B.GetLength(1))
+        {
+            Debug.Log("Bの列数とAの行数が一致します");
+
+        }
+        else
+        {
+            Debug.Log("Bの列数とAの行数が一致しません");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
