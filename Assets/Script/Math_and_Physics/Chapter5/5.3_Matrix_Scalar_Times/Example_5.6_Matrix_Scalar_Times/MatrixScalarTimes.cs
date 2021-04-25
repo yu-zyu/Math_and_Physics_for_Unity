46b0c7fb198b886acd54472477a4bf98391a60d3
yu-zyu <siragaigarasi@gmail.com>
2021-04-26 02:31:09 +0900

    行列のスカラー倍をUnityで再現した

---
+++ Assets/Script/Math_and_Physics/Chapter5/5.3_Matrix_Scalar_Times/Example_5.6_Matrix_Scalar_Times/MatrixScalarTimes.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MatrixScalarTimes : MonoBehaviour
+{
+    public float[,] B = new float[,] {
+        { 3, 6, -4 },
+        { 0, -1, 2 }
+    };
+
+    public float[,] C = new float[,] {
+        { 0, 0, 0 },
+        { 0, 0, 0 }
+    };
+
+    public float scale;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 2; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                C[i, j] = B[i, j] * scale;
+                Debug.Log(C[i, j]);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
