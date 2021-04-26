dafb0ef8a905a6c728e72e0cec68309e7f3d6560
yu-zyu <siragaigarasi@gmail.com>
2021-04-27 01:14:19 +0900

    転置ベクトルをUnityで再現した

---
+++ Assets/Script/Math_and_Physics/Chapter5/5.5_Transpose_of_Matrix/Example_5.10_Transpose_of_Matrix/TransposeMatrixof3X3TypeMatrix.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransposeMatrixof3X3TypeMatrix : MonoBehaviour
+{
+    public float[,] B = new float[,] {
+        { 1, 4, 7 },
+        { 2, 5, 8 },
+        { 3, 6, 9 }
+    };
+
+    public float[,] X = new float[,] {
+        { 0, 0, 0 },
+        { 0, 0, 0 },
+        { 0, 0, 0 }
+    };
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                X[i, j] = B[j, i];
+            }
+        }
+
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                Debug.Log(X[i, j]);
+
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Script/Math_and_Physics/Chapter5/5.5_Transpose_of_Matrix/Example_5.11_Transpose_Vector/TransposeVector.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransposeVector : MonoBehaviour
+{
+    public float[,] C = new float[,] {
+        { 1 },
+        { 2 },
+        { 3 }
+    };
+
+    public float[,] X = new float[,] {
+        { 0, 0, 0 }
+    };
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+                X[0, i] = C[i, 0];
+        }
+
+        for (int i = 0; i < 3; i++)
+        {
+            Debug.Log(X[0,i]);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
