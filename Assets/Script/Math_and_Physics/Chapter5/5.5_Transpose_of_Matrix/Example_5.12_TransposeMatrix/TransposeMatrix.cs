db1b8ab976753cbcf583862af036aedaf75a2991
yu-zyu <siragaigarasi@gmail.com>
2021-04-27 23:26:51 +0900

    行列の足し算による3次元の平行移動

---
+++ Assets/Script/Math_and_Physics/Chapter5/5.5_Transpose_of_Matrix/Example_5.12_TransposeMatrix/TransposeMatrix.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransposeMatrix : MonoBehaviour
+{
+    public float[,] D = new float[,] {
+        { 2, -1, 3, 8 },
+        { 0,  5, 1, 4 }
+    };
+
+    public float[,] X = new float[,] {
+        { 0, 0 },
+        { 0, 0 },
+        { 0, 0 },
+        { 0, 0 }
+    };
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 2; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                X[j,i] = D[i,j];
+            }
+        }
+
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j < 2; j++)
+            {
+                Debug.Log(X[i, j]);
+
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Script/Math_and_Physics/Chapter6/6.1_Translation/Example_6.1_Two-Dimensional_Translation_By_Matrix_Addition/TwoDimensionalTranslationMatrixAddition.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TwoDimensionalTranslationMatrixAddition : MonoBehaviour
+{
+    private Material _material;
+    public float[] A = new float[2];
+    public float[] B = new float[2];
+    public float[] C = new float[2];
+    public bool isMove;
+    public float[] addMatrix = new float[2];
+
+    private Mesh _mesh;
+
+    // (1) 頂点座標（この配列のインデックスが頂点インデックス）
+    public Vector3[] _positions = new Vector3[]{
+        new Vector3(0, 0, -1),
+        new Vector3(0, 0, -1),
+        new Vector3(0, 0, -1)
+    };
+
+
+    // (2) ポリゴンを形成する頂点インデックスを順番に指定する
+    private int[] _triangles = new int[] { 0, 1, 2 };
+
+    // (3) 法線
+    private Vector3[] _normals = new Vector3[]{
+        new Vector3(0, 0, -1),
+        new Vector3(0, 0, -1),
+        new Vector3(0, 0, -1)
+    };
+
+    private void Awake()
+    {
+        _mesh = new Mesh();
+    }
+
+    private void Update()
+    {
+
+        if (isMove)
+        {
+            for(int i =0; i<2; i++)
+            {
+                A[i] += addMatrix[i];
+                B[i] += addMatrix[i];
+                C[i] += addMatrix[i];
+            }
+            isMove = false;
+        }
+
+        
+        _positions[0] = new Vector3(A[0], A[1], 0);
+        _positions[1] = new Vector3(B[0], B[1], 0);
+        _positions[2] = new Vector3(C[0], C[1], 0);
+
+        // (4) Meshに頂点情報を代入
+        _mesh.vertices = _positions;
+        _mesh.triangles = _triangles;
+        _mesh.normals = _normals;
+
+        _mesh.RecalculateBounds();
+
+        // (5) 描画
+        Graphics.DrawMesh(_mesh, Vector3.zero, Quaternion.identity, _material, 0);
+    }
+}
+
