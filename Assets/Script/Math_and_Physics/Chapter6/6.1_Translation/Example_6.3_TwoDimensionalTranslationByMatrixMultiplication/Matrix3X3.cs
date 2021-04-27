0647fa47f6b78a92311ef5f260b934bd61623855
yu-zyu <siragaigarasi@gmail.com>
2021-04-28 02:14:20 +0900

    行列の掛け算による2次元の平行移動をUnityで再現した

---
+++ Assets/Script/Math_and_Physics/Chapter6/6.1_Translation/Example_6.3_TwoDimensionalTranslationByMatrixMultiplication/Matrix3X3.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Matrix3X3
+{
+    public float[,] matrix = new float[,] {
+        { 0, 0, 0 },
+        { 0, 0, 0 },
+        { 0, 0, 0 }
+    };
+}
