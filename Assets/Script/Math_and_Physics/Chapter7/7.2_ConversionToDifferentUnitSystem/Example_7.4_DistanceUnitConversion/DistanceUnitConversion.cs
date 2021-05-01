330cca59f0201b48317484c8ec2cec6e2e28ac59
yu-zyu <siragaigarasi@gmail.com>
2021-05-02 04:48:51 +0900

    距離の単位変換をUnityで再現した

---
+++ Assets/Script/Math_and_Physics/Chapter7/7.2_ConversionToDifferentUnitSystem/Example_7.4_DistanceUnitConversion/DistanceUnitConversion.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DistanceUnitConversion : MonoBehaviour
+{
+    public float mil;
+    public float result;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        result = UnitConversion.ConvertMilesToMeters(mil);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
