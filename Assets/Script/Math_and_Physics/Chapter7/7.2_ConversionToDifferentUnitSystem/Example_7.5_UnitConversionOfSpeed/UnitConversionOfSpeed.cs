ae641c2a9605d2e59b5243729ea3759ba4380152
yu-zyu <siragaigarasi@gmail.com>
2021-05-02 04:59:28 +0900

    速さの単位変換をUnityで再現した

---
+++ Assets/Script/Math_and_Physics/Chapter7/7.2_ConversionToDifferentUnitSystem/Example_7.5_UnitConversionOfSpeed/UnitConversionOfSpeed.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitConversionOfSpeed : MonoBehaviour
+{
+    public float mi;
+    public float result;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        result = UnitConversion.ConvertSpeed(mi);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
