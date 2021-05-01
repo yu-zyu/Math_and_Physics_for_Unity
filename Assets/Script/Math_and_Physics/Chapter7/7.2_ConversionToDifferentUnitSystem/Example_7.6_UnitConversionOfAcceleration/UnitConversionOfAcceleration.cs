c4360e2ccd5500f1b44772510ddae3ec88f10eab
yu-zyu <siragaigarasi@gmail.com>
2021-05-02 05:06:30 +0900

    加速度の単位変換をUnityで再現した

---
+++ Assets/Script/Math_and_Physics/Chapter7/7.2_ConversionToDifferentUnitSystem/Example_7.6_UnitConversionOfAcceleration/UnitConversionOfAcceleration.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitConversionOfAcceleration : MonoBehaviour
+{
+    public float mi;
+    public float result;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        result = UnitConversion.ConvertAcelMilesToMeters(mi);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
