653ee68924b7096e4e1e4104dbcba67a4facb07d
yu-zyu <siragaigarasi@gmail.com>
2021-05-02 02:30:34 +0900

    メートルからキロメートルへの変換をUnityで再現した

---
+++ Assets/Script/Math_and_Physics/Chapter7/7.1_MetricSystem/Example_7.1_ConversionFromMetersToKilometers/ConversionFromMetersToKilometers.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConversionFromMetersToKilometers : MonoBehaviour
+{
+    public float meter;
+    public float kiloMeters;
+    public float resultKiloMeters;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        float kilo = meter * 0.01f;
+        resultKiloMeters = kilo + kiloMeters;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
