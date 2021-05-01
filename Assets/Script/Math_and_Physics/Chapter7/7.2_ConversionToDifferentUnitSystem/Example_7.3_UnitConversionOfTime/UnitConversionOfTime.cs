a61c6e9dd6708ed14d799fcc22b1e76002cc1f85
yu-zyu <siragaigarasi@gmail.com>
2021-05-02 04:40:25 +0900

    時間の単位変換をUnityで再現した

---
+++ Assets/Script/Math_and_Physics/Chapter7/7.2_ConversionToDifferentUnitSystem/Example_7.3_UnitConversionOfTime/UnitConversionOfTime.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitConversionOfTime : MonoBehaviour
+{
+    public int week;
+    public float result;
+        
+    // Start is called before the first frame update
+    void Start()
+    {
+        float day = week * 7;
+        result = UnitConversion.ConvertDaysToSeconds(day);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Script/Math_and_Physics/Static/UnitConversion.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class UnitConversion 
+{
+    public const float MILE_TO_KM = 1.609f;
+    public const float KN_TO_METER = 1000;
+    public const int WEEK_TO_DAY = 7;
+    public const int DAY_TO_HOUR = 24;
+    public const int HOUR_TO_MIN = 60;
+    public const int MIN_TO_SEC = 60;
+
+    public static float ConvertDaysToSeconds(float days)
+    {
+        float seconds;
+        seconds = days * DAY_TO_HOUR * HOUR_TO_MIN * MIN_TO_SEC;
+        return seconds;
+    }
+    public static float ConvertMilesToMeters(float miles)
+    {
+        float meters;
+        meters = miles * MILE_TO_KM * KN_TO_METER;
+        return meters;
+    }
+    public static float ConvertSpeed(float miles)
+    {
+        float meters;
+        meters = (miles * MILE_TO_KM * KN_TO_METER) / (HOUR_TO_MIN * MIN_TO_SEC);
+        return meters;
+    }
+    public static float ConvertAcelMilesToMeters(float miles)
+    {
+        float accelMeters;
+        accelMeters = (miles * MILE_TO_KM * KN_TO_METER) /
+            (HOUR_TO_MIN * MIN_TO_SEC * HOUR_TO_MIN * MIN_TO_SEC);
+        return accelMeters;
+    }
+}
