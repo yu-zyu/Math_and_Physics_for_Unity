7c28f31aca5656c079bc2e734060d4537c5af9d5
yu-zyu <siragaigarasi@gmail.com>
2021-05-02 02:48:51 +0900

    キロメートルからメートルへｎ変換をUnityで再現した

--- Assets/Script/Math_and_Physics/Chapter7/7.1_MetricSystem/Example_7.1_7.2_ConversionFromMetersToKilometers/ConversionFromMetersToKilometers.cs
+++ Assets/Script/Math_and_Physics/Chapter7/7.1_MetricSystem/Example_7.1_7.2_ConversionFromMetersToKilometers/ConversionFromMetersToKilometers.cs
@@ -7,17 +7,23 @@ public class ConversionFromMetersToKilometers : MonoBehaviour
     public float meter;
     public float kiloMeters;
     public float resultKiloMeters;
+    public float resultMeters;
 
     // Start is called before the first frame update
     void Start()
     {
-        float kilo = meter * 0.01f;
+        float kilo = meter * 0.001f;
         resultKiloMeters = kilo + kiloMeters;
+        resultMeters = resultKiloMeters * 1000; 
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void ConvertToMeter()
+    {
     }
 }
