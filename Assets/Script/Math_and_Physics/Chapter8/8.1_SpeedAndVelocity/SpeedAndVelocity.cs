77d964016e72b163efbf5a7b693f2a0023ec79dd
yu-zyu <siragaigarasi@gmail.com>
2021-05-02 21:32:51 +0900

    速度が一定のときの変位の求め方をUnityで再現した

---
+++ Assets/Script/Math_and_Physics/Chapter8/8.1_SpeedAndVelocity/SpeedAndVelocity.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedAndVelocity : MonoBehaviour
+{
+
+    public float D;
+    public float v;
+    public float t;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        D = v * t;
+        gameObject.transform.position += new Vector3(D,0,0);
+    }
+}
