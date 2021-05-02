25465d223f74f01bfd6d9fba617dae667f21fea1
yu-zyu <siragaigarasi@gmail.com>
2021-05-02 23:34:38 +0900

    速度が一定のときの新しい位置の求め方をUnityで再現した

---
+++ Assets/Script/Math_and_Physics/Chapter8/8.1_SpeedAndVelocity/Example_8.2_HowToFindNewPositionWhenTheSpeedIsConstant/HowToFindNewPositionWhenTheSpeedIsConstant.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HowToFindNewPositionWhenTheSpeedIsConstant : MonoBehaviour
+{
+
+    public float D;
+    public float v;
+    public float t;
+    public float targetPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        D = targetPos + v * t;
+        gameObject.transform.position += new Vector3(D,0,0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
