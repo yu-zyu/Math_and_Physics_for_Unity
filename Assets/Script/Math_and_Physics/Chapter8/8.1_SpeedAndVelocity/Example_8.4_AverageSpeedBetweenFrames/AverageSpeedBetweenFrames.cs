8c9b1cd67c0dbb888ee6fa297217b6f6c292fe26
yu-zyu <siragaigarasi@gmail.com>
2021-05-03 00:17:01 +0900

    フレーム間の平均速度をUnityで再現した

---
+++ Assets/Script/Math_and_Physics/Chapter8/8.1_SpeedAndVelocity/Example_8.4_AverageSpeedBetweenFrames/AverageSpeedBetweenFrames.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AverageSpeedBetweenFrames : MonoBehaviour
+{
+    public float v;
+    public float xf;
+    public float xi;
+    public float t;
+    // Start is called before the first frame update
+    void Start()
+    {
+        v = (xf - xi) / t;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
