bcc2e2997bad0fee4b6ef8ced492b33e2006e59f
yu-zyu <siragaigarasi@gmail.com>
2021-05-02 23:46:01 +0900

    平均速度の計算をUnityで再現した

---
+++ Assets/Script/Math_and_Physics/Chapter8/8.1_SpeedAndVelocity/Example_8.3_AverageSpeedCalculation/AverageSpeedCalculation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AverageSpeedCalculation : MonoBehaviour
+{
+    public float v;
+    public float xf;
+    public float xi;
+    public float t;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        v = (xf - xi) / t;
+        v = v * 60 * 60;
+    }
+}
