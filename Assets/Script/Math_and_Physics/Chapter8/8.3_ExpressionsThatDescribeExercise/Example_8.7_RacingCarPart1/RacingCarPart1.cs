3569d9cb45a4a7d1b92b2b738f1a0be91c74442d
yu-zyu <siragaigarasi@gmail.com>
2021-05-03 19:43:34 +0900

    レーシングカーその１をUnityで再現した

---
+++ Assets/Script/Math_and_Physics/Chapter8/8.3_ExpressionsThatDescribeExercise/Example_8.7_RacingCarPart1/RacingCarPart1.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RacingCarPart1 : MonoBehaviour
+{
+
+    public float vf;
+    public float vi;
+    public float a;
+    public float t;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        t = (-vi -vf) / a;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
