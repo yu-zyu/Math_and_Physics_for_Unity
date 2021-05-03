fcf197b4fc2d9bfae06312fbdc8bcf53a0be0cb4
yu-zyu <siragaigarasi@gmail.com>
2021-05-03 09:46:54 +0900

    加速度の計算をUnityで再現した

---
+++ Assets/Script/Math_and_Physics/Chapter8/8.2_Acceleration/Example_8.5_8.6_AccelerationAlculation/AccelerationCalculation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AccelerationCalculation : MonoBehaviour
+{
+    public float vf;
+    public float vi;
+    public float tf;
+    public float ti;
+    public float a1;
+    public float a2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        a1 = (vf - vi) / ((tf - ti) /(60 * 60));
+        a2 = UnitConversion.ConvertAcelMilesToMeters(a1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
