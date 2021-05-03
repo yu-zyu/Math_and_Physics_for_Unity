9ee568b594f467790aa852116e7022b2ac615224
yu-zyu <siragaigarasi@gmail.com>
2021-05-03 20:59:02 +0900

    鉛直方向の運動をUnityで再現した

---
+++ Assets/Script/Math_and_Physics/Chapter8/8.3_ExpressionsThatDescribeExercise/Example_8.10_VerticalMovement/VerticalMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VerticalMovement : MonoBehaviour
+{
+    public float vf;
+    public float vi;
+    public float a;
+    public float t2;
+    public float t;
+    public float x;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        a = a / 2;
+        t2 = (x + vi) / a;
+        t = Mathf.Sqrt(t2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
