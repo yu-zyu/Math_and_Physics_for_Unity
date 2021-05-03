a870f3512d6819d263586d2c4cb797633c9179ca
yu-zyu <siragaigarasi@gmail.com>
2021-05-03 20:38:07 +0900

    2つ以上の士気を使うケースをUnityで再現した

---
+++ Assets/Script/Math_and_Physics/Chapter8/8.3_ExpressionsThatDescribeExercise/Example_8.9_CasesThatUseTwoOrMoreFormulas/CasesThatUseTwoOrMoreFormulas.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CasesThatUseTwoOrMoreFormulas : MonoBehaviour
+{
+    public float vf;
+    public float vi;
+    public float a;
+    public float t;
+    public float x;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        vf = Mathf.Sqrt(Mathf.Pow(vi, 2) + (2*a)*(x));
+        t = (vf - vi) / a;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
