9b553eeb30a3f271dea7d76428d6e811614c38e8
yu-zyu <siragaigarasi@gmail.com>
2021-05-06 01:08:36 +0900

    瞬間加速度の求め方をUnityで再現した

---
+++ Assets/Script/Math_and_Physics/Chapter9/9.2_AccelerationAndQuadraticDerivativeSeenInTheGraph/Example_9.5_HowToFindTheInstantaneousAcceleration/HowToFindTheInstantaneousAcceleration.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HowToFindTheInstantaneousAcceleration : MonoBehaviour
+{
+    public float time;
+    public float timeLimit;
+    public int timeCount;
+    public int a;
+    public int b;
+    public int h;
+    public float averageAcceleration;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        time += Time.deltaTime;
+        if (timeLimit < time)
+        {
+            time = 0;
+            timeCount++;
+            timeCount = 0;
+        }
+
+
+        averageAcceleration = (f(3 + h) - f(3)) / h;
+        
+        
+    }
+
+    float f(float t)
+    {
+        return -9.8f * t + 25;
+    }
+}
