c86e79524704aba28e1b59319397fad6c2fbb202
yu-zyu <siragaigarasi@gmail.com>
2021-05-09 19:01:35 +0900

    3次元における平均速度の求め方をUnityで再現した

---
+++ Assets/Script/Math_and_Physics/Chapter10/10.1_VectorSpecifications/Example_10.3_HowToFindTheAverageVelocityIn3D/HowToFindTheAverageVelocityIn3D.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HowToFindTheAverageVelocityIn3D : MonoBehaviour
+{
+    public Transform targetA;
+    public Transform targetB;
+    public Vector3 displacement;
+    public Vector3 averageSpeed;
+    public float time;
+    public float deltaTime;
+    public bool flg = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+            displacement = targetB.position - targetA.position;
+            averageSpeed = displacement / time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        deltaTime += Time.deltaTime;
+        if(1 <= deltaTime)
+        {
+            if(targetA.position == targetB.position)
+            {
+                flg = true;
+            }
+
+            if (!flg)
+            {
+                targetA.position += averageSpeed;
+            }
+            deltaTime = 0;
+        }
+    }
+}
