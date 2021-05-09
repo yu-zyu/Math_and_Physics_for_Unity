842a94ab96823735ebbe46d5c0df45b99943d028
yu-zyu <siragaigarasi@gmail.com>
2021-05-10 06:51:44 +0900

    運動を記述する式の2次元への応用をUnityで再現した

---
+++ Assets/Script/Math_and_Physics/Chapter10/10.1_VectorSpecifications/Example_10.4_Two-dimensionalApplicationOfEquationsThatDescribeMotion/EquationsThatDescribeMotion.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EquationsThatDescribeMotion : MonoBehaviour
+{
+    public Vector3 vf;
+    public Vector3 vi;
+    public float t;
+    public Vector3 a;
+    public Transform car;
+    public float deltaTime;
+    public bool isChange;
+    // Start is called before the first frame update
+    void Start()
+    {
+        vf = vi + (a * t);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        deltaTime += Time.deltaTime;
+        if (1 <= deltaTime)
+        {
+            if (isChange)
+            {
+                car.position += vf;
+
+            }
+            else
+            {
+                car.position += vi;
+            }
+
+            deltaTime = 0;
+        }
+    }
+}
