cbdc0ef58face7b7287607221d98bdce3f19b4ca
yu-zyu <siragaigarasi@gmail.com>
2021-05-09 13:31:00 +0900

    2次元3次元における変位の求め方をUnityで再現した

---
+++ Assets/Script/Math_and_Physics/Chapter10/10.1_VectorSpecifications/Example_10.1_10.2_HowToFindTheDisplacementInTwoDimensions/DisplacementInTwoDimensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplacementInTwoDimensions : MonoBehaviour
+{
+    public Transform targetA;
+    public Transform targetB;
+    public Vector3 displacement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        displacement = targetB.position - targetA.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
