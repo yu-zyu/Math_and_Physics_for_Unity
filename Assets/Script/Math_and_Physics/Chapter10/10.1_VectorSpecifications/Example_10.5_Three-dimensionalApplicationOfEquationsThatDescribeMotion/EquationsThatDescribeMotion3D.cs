b350ffeb74b8878fb928c52070ab239d2c22bbc5
yu-zyu <siragaigarasi@gmail.com>
2021-05-10 08:52:35 +0900

    運動を記述する式の3次元への応用をUnityで再現した

---
+++ Assets/Script/Math_and_Physics/Chapter10/10.1_VectorSpecifications/Example_10.5_Three-dimensionalApplicationOfEquationsThatDescribeMotion/EquationsThatDescribeMotion3D.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EquationsThatDescribeMotion3D : MonoBehaviour
+{
+    public Vector3 x;
+    public Vector3 vi;
+    public float t;
+    public Vector3 a;
+    public Transform car;
+    public float deltaTime;
+    public float time;
+    // Start is called before the first frame update
+    void Start()
+    {
+        x = (vi * t) + (a * Mathf.Pow(t, 2)) / 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       // deltaTime += Time.deltaTime;
+       // if (1 <= deltaTime)
+       // {
+       //     time++;
+       //     car.position += a;
+       //     deltaTime = 0;
+       // }
+    }
+}
