2cdffd98c413453fea1750dd2e9a3088fe769651
yu-zyu <siragaigarasi@gmail.com>
2021-05-16 20:35:57 +0900

    投げ上げのサンプルを試した

---
+++ Assets/Script/Math_and_Physics/Chapter10/10.1_VectorSpecifications/Example_10.5_Three-dimensionalApplicationOfEquationsThatDescribeMotion/colision.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class colision : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(collision.gameObject.transform.position); // ログを表示する
+    }
+
+}
---
+++ Assets/Script/Math_and_Physics/Chapter10/10.2_Projection/Example_10.8_FallFromKnownHeight/ridgidSample/ThrowUp.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowUp : MonoBehaviour
+{
+	public Vector3 vec;
+	private Rigidbody rigid;
+    public float topTime;
+
+    void Start()
+	{
+		rigid = GetComponent<Rigidbody>();
+	}
+
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			rigid.velocity = vec;
+			topTime = vec.y / (-Physics.gravity.y);
+			StartCoroutine(WaitBreak(topTime));
+
+			Debug.Log(topTime.ToString("F2") + "秒後に最高点("+ gameObject.transform.position.y +")に達します");
+		}
+	}
+
+	private IEnumerator WaitBreak(float _waitTime)
+	{
+		yield return new WaitForSeconds(_waitTime);
+		Debug.Break();
+	}
+}
