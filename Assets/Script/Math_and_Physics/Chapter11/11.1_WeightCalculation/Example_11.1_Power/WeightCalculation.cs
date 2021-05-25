82ff3e212a922ddb3466fad100988a4b637a56f9
yu-zyu <siragaigarasi@gmail.com>
2021-05-25 20:17:59 +0900

    重さの計算をUnityで再現した

---
+++ Assets/Script/Math_and_Physics/Chapter11/11.1_WeightCalculation/Example_11.1_Power/WeightCalculation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeightCalculation : MonoBehaviour
+{
+    public float w;
+    public float m;
+    public float g;
+    public float lbs;
+    public float n;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        w = m * g;
+        lbs = w * n;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
