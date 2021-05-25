674b7aa58e8f25d52a2f6fee189b5e3b41985f6c
yu-zyu <siragaigarasi@gmail.com>
2021-05-25 20:36:19 +0900

    質量の計算をUnityで再現した

---
+++ Assets/Script/Math_and_Physics/Chapter11/11.1_WeightCalculation/Example_11.2_CalculationOfMass/CalculationOfMass.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CalculationOfMass : MonoBehaviour
+{
+    public float lbs;
+    public float w;
+    public float m;
+    public float g;
+    public float n;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        w = lbs / n;
+        m = w / g ;
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
