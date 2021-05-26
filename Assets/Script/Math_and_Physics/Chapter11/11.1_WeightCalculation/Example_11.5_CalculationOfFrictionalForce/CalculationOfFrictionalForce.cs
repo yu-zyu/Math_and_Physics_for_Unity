20c6484e07571c79cc841ae4335d099b3c5c00a5
yu-zyu <siragaigarasi@gmail.com>
2021-05-27 04:14:37 +0900

    摩擦力の計算をUnityで再現した

---
+++ Assets/Script/Math_and_Physics/Chapter11/11.1_WeightCalculation/Example_11.5_CalculationOfFrictionalForce/CalculationOfFrictionalForce.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CalculationOfFrictionalForce : MonoBehaviour
+{
+
+    public float lbs;
+    public float n1;
+    public float n;
+    public float normalForce;
+    public float staticFunction;
+    public float dynamicFunction;
+    public float us;
+    public float uk;
+    public float fs;
+    public float fk;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        n = lbs / n1;
+        normalForce = Mathf.Abs(n);
+        fs = us * n;
+        fk = uk * n;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
