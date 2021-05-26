eafd6c7a27c549e7c1f78bd8de52be85f7f49472
yu-zyu <siragaigarasi@gmail.com>
2021-05-26 12:50:56 +0900

    斜面の垂直抗力をUnityで再現した

---
+++ Assets/Script/Math_and_Physics/Chapter11/11.1_WeightCalculation/Example_11.3_NormalForceOnHorizontalSurface/NormalForceOnHorizontalSurface.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NormalForceOnHorizontalSurface : MonoBehaviour
+{
+    public float lbs;
+    public float n;
+    public float n2;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        n = lbs / n;
+        n2 = Mathf.Abs(n);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Script/Math_and_Physics/Chapter11/11.1_WeightCalculation/Example_11.4_NormalForceOnTheSlope/NormalForceOnTheSlope.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NormalForceOnTheSlope : MonoBehaviour
+{
+    public float n;
+    public float n1;
+    public float lbs;
+    public float angle;
+    public float normalForce;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        n = lbs / n1;
+        normalForce = n * Mathf.Cos(angle * Mathf.Deg2Rad);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
