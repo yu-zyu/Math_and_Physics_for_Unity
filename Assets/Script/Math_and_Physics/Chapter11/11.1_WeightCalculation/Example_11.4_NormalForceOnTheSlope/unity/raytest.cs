2d8627b5a9abebd5a6ce8a21cb5782ccd71920fb
yu-zyu <siragaigarasi@gmail.com>
2021-05-31 05:18:01 +0900

    法線ベクトルと重力ベクトルで内積を計算した

---
+++ Assets/Script/Math_and_Physics/Chapter11/11.1_WeightCalculation/Example_11.4_NormalForceOnTheSlope/unity/raytest.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class raytest : MonoBehaviour
+{
+    public GameObject plane;
+    public GameObject sphere;
+    public Vector3 rag;
+    public float ragf;
+    public float deg;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        toMostUp(plane, 1, 1);
+        toMostUp(sphere, 1, 1);
+
+        //rag = plane.transform.up * sphere.transform.up;
+        //ragf = ( plane.transform.up.x * sphere.transform.up.x)+ ( plane.transform.up.y * sphere.transform.up.y) + ( plane.transform.up.z * sphere.transform.up.z);
+        ragf = Vector3.Dot(plane.transform.up, sphere.transform.up);
+        deg = Mathf.Acos(ragf) * Mathf.Rad2Deg;
+        Debug.Log(deg);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+
+        
+    }
+
+    public void toMostUp(GameObject obj, float y, int distance)
+    {
+        // レイを真上へ(Vector3.up)
+        Ray ray = new Ray(obj.transform.position, obj.transform.up);
+ Debug.DrawRay(ray.origin, ray.direction * 1130.0f, Color.red, 5f, false);
+    }
+ 
+}
---
+++ Assets/Script/Math_and_Physics/Chapter11/11.1_WeightCalculation/Example_11.6_HowToCalculateTheResultantForceInTwoDimensions/ResultantForceInTwoDimensions.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResultantForceInTwoDimensions : MonoBehaviour
+{
+    public float n1;
+    public Vector2 fr;
+    public float pull;
+    public float pullAngle;
+    public float fk;
+    public float n;
+    public float u;
+    public float lbs;
+    public Vector2 w;
+    public Vector2 normalForce;
+    public Vector2 combinedForce;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        w = new Vector2(0, lbs / n1);
+        fr = new Vector2(pull * Mathf.Cos(pullAngle * Mathf.Deg2Rad), pull * Mathf.Sin(pullAngle * Mathf.Deg2Rad));
+        normalForce = w + fr;
+        ///combinedForce = w + fr + n + fk;
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
