1ebd83cadb4640f8af721b1ad195ab24d2de61b3
yu-zyu <siragaigarasi@gmail.com>
2021-06-01 08:20:56 +0900

    力を斜めに加えた場合の仕事の計算をUnityで再現した

---
+++ Assets/Script/Math_and_Physics/Chapter12/12.1_WorkAndKineticEnergy/Example_12.2_ForceIsAppliedDiagonally/ForceIsAppliedDiagonally.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForceIsAppliedDiagonally : MonoBehaviour
+{
+    public float f;
+    public float fx;
+    public float angle;
+    public float combinedForce;
+    public float motionFriction;
+    public float w;
+    public float deltaX;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fx = f * Mathf.Cos(angle * Mathf.Deg2Rad);
+        combinedForce = fx - motionFriction;
+        w = combinedForce * deltaX;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
