c720c472e998df58d7a0878fd826636658d2434f
yu-zyu <siragaigarasi@gmail.com>
2021-05-31 09:22:02 +0900

    ニュートンの運動の第二法則をUnitｙで再現した

---
+++ Assets/Script/Math_and_Physics/Chapter11/11.2_EffectOfForceOnTheMotionOfAnObject_ApplicationOfNewton'sLawOfMotion/Example_11.8_Newton'sSecondLawOfMovement/NewtonsSecondLawOfMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewtonsSecondLawOfMovement : MonoBehaviour
+{
+    public float w;
+    public float m;
+    public float g;
+    public Vector2 a;
+    public Vector2 fnet;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //w = mg
+        m = w / g;
+
+        // fnet = m * a;
+        a = fnet / m;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
