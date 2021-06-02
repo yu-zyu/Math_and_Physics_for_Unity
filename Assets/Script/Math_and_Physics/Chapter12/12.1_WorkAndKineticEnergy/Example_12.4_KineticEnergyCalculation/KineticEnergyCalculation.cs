68733ac4a0121505fabd66778bab5dfb5b879cdf
yu-zyu <siragaigarasi@gmail.com>
2021-06-03 07:27:36 +0900

    運動エネルギーの計算をUnityで再現した

---
+++ Assets/Script/Math_and_Physics/Chapter12/12.1_WorkAndKineticEnergy/Example_12.4_KineticEnergyCalculation/KineticEnergyCalculation.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KineticEnergyCalculation : MonoBehaviour
+{
+    public float mi;
+    public float m;
+    public float g;
+    public float kg;
+    public float ke;
+    public float v;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        v = (mi * 1609) / (60 * 60);
+        kg = m / 1000;
+
+        //KE = 1/2mv^2
+        ke = 0.5f * kg * Mathf.Pow(v, 2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
