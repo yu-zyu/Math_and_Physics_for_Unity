ea28ab7885abfaf6f5387cc6afa8ee71d1ced3fa
yu-zyu <siragaigarasi@gmail.com>
2021-06-03 07:53:57 +0900

    仕事とエネルギーの定理をUnityで再現した

---
+++ Assets/Script/Math_and_Physics/Chapter12/12.1_WorkAndKineticEnergy/Example_12.5_WorkEergyTheorem/WorkEergyTheorem.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorkEergyTheorem : MonoBehaviour
+{
+    public float f;
+    public float deltaX;
+    public float m;
+    public float j;
+    public float vf;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m = m / 1000;
+        j = f * deltaX;
+        vf = Mathf.Sqrt(j / (0.5f * m));
+       
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
