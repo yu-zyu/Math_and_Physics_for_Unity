3da69bede9f9b03dee73969574cb556bcd313a0e
yu-zyu <siragaigarasi@gmail.com>
2021-06-03 09:23:23 +0900

    重力による位置エネルギーをUnityで再現した

---
+++ Assets/Script/Math_and_Physics/Chapter12/12.2_PotentialEnergyAndConservationLaw/Example_12.6_PotentialEnergyDueToGravity/PotentialEnergyDueToGravity.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PotentialEnergyDueToGravity : MonoBehaviour
+{
+    public float lbs;
+    public float n1;
+    public float n;
+    public float w;
+    public float m;
+    public float g;
+    public float pe;
+    public float y;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        n = lbs / n1;
+        m = n / g;
+        pe = m * g * y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
