8115fa28d8217d8fbac091125dc13e0de038b14d
yu-zyu <siragaigarasi@gmail.com>
2021-06-04 06:15:25 +0900

    力学的エネルギー保存の法則をUniytで再現した

---
+++ Assets/Script/Math_and_Physics/Chapter12/12.2_PotentialEnergyAndConservationLaw/Example_12.7_LawOfMechanicalEnergyConservation/LawOfMechanicalEnergyConservation.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LawOfMechanicalEnergyConservation : MonoBehaviour
+{
+    public float lbs;
+    public float n1;
+    public float n;
+    public float m;
+    public float g;
+    public float yi;
+    public float yf;
+    public float vi;
+    public float vf;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        n = lbs / n1;
+        m = n / g;
+
+        vf = Mathf.Sqrt(((0.5f * m + Mathf.Pow(vi, 2)) + (m * g * yi)) / ((0.5f * m) + (m * g * yf)));
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
