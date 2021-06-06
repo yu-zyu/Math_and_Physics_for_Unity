2bf0a9ba777b216ae57594e78855fb1ee4861add
yu-zyu <siragaigarasi@gmail.com>
2021-06-06 16:09:13 +0900

    座標軸に平行でない場合の2次元のベクトル反射をUnityで再現した

---
+++ Assets/Script/Math_and_Physics/Chapter13/13.1_CollisionOfFixedObjects/Example_13.2_NonParallelVectorReflection/NonParallelVectorReflection.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NonParallelVectorReflection : MonoBehaviour
+{
+    public Vector2 boundary;
+    public Vector2 normal;
+    public Vector2 b;
+    public Vector2 ballStart;
+    public Vector2 ballEnd;
+    public float size;
+    public Vector2 normalDash;
+    public Vector2 p;
+    public Vector2 vi;
+    public Vector2 vf;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        boundary = ballEnd - ballStart;
+        normal = new Vector2(boundary.y, -boundary.x);
+        size = Mathf.Sqrt(Mathf.Pow(normal.x, 2) + Mathf.Pow(normal.y, 2));
+        normalDash = new Vector2(normal.x / size, normal.y / size);
+        p = Vector2.Dot(-vi, normalDash) * normalDash;
+        vf = 2 * p + vi;
+
+
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
