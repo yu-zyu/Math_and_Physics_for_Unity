26900edac06971f75269a0b4abe9ba6ef3aef640
yu-zyu <siragaigarasi@gmail.com>
2021-06-06 09:26:05 +0900

    座標軸に平行な場合とそうでない場合のベクトル反射をUnityで再現した

---
+++ Assets/Script/Math_and_Physics/Chapter13/13.1_CollisionOfFixedObjects/Example_13.1_VectorReflectionWhenParallelToTheAxesAndWhenNot/VectorReflection.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VectorReflection : MonoBehaviour
+{
+    public Vector2 col;
+    public Vector2 colReflect;
+    // Start is called before the first frame update
+    void Start()
+    {
+        colReflect = new Vector2(-col.x, col.y);
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
