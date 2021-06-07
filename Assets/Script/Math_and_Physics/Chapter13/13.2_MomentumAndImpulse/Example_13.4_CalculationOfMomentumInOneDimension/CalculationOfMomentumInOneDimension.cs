7c3eb3e208b16db33b5a62e997140e17a240f211
yu-zyu <siragaigarasi@gmail.com>
2021-06-08 07:37:55 +0900

    3次元における運動量の計算をUnityで再現した

---
+++ Assets/Script/Math_and_Physics/Chapter13/13.2_MomentumAndImpulse/Example_13.4_CalculationOfMomentumInOneDimension/CalculationOfMomentumInOneDimension.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CalculationOfMomentumInOneDimension : MonoBehaviour
+{
+    public float lbs;
+    public float n1;
+    public float mi;
+    public float v;
+    public float w;
+    public float m;
+    public float g;
+    public float p;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        w = -lbs / n1;
+        m = w / g;
+        v = mi * 1609 / 3600;
+        p = m * v;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Script/Math_and_Physics/Chapter13/13.2_MomentumAndImpulse/Example_13.5_CalculationOfMomentum3D/CalculationOfMomentum3D.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CalculationOfMomentum3D : MonoBehaviour
+{
+    public float m;
+    public Vector3 v;
+    public Vector3 p;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        p = m * v;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
