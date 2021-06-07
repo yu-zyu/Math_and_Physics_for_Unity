f8879a7167e8cb93c0614792d22547882f59235c
yu-zyu <siragaigarasi@gmail.com>
2021-06-08 08:06:41 +0900

    力積と運動量の定理をUnityで再現した

---
+++ Assets/Script/Math_and_Physics/Chapter13/13.2_MomentumAndImpulse/Example_13.6_ImpulseAndMomentumTheorem/ImpulseAndMomentumTheorem.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImpulseAndMomentumTheorem : MonoBehaviour
+{
+    public float m;
+    public Vector3 impulse;
+    public Vector3 v;
+    public Vector3 vi;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       v = (impulse - (m * vi)) / m;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
