542997dfcde38502444e48749b8a908ca5a58b94
yu-zyu <siragaigarasi@gmail.com>
2021-06-09 07:22:34 +0900

    完全非弾性衝突をUnityで再現した

---
+++ Assets/Script/Math_and_Physics/Chapter13/13.3_CollisionModeling/Example_13.7_LawOfConservationOfMomentum/LawOfConservationOfMomentum.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LawOfConservationOfMomentum : MonoBehaviour
+{
+    public float m1;
+    public Vector3 v1i;
+    public float m2;
+    public Vector3 v2i;
+    public Vector3 v1f;
+    public Vector3 v2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        v2f = ((m1 * v1i) + (m2 * v2i) - (m1 * v1f)) / m2; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Script/Math_and_Physics/Chapter13/13.3_CollisionModeling/Example_13.8_CompletelyInelasticCollision/CompletelyInelasticCollision.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CompletelyInelasticCollision : MonoBehaviour
+{
+    public float p1lbs;
+    public float p2lbs;
+    public float n1;
+    public float m1;
+    public float w;
+    public float g;
+    public float m2;
+    public Vector2 v1i;
+    public Vector2 v2i;
+    public Vector2 v1f;
+    public Vector2 v2f;
+    public Vector2 vf;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        w = p1lbs / n1;
+        m1 = w / g;
+
+        w = p2lbs / n1;
+        m2 = w / g;
+
+        vf = ((m1 * v1i) + (m2 * v2i)) / (m1 + m2);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
