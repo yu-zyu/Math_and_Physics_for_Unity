f19fe7c1635fb02c7fcbfcf2392a5719bfa248c9
yu-zyu <siragaigarasi@gmail.com>
2021-06-11 07:54:47 +0900

    CD-ROMの平均角速度をUnityで再現した

---
+++ Assets/Script/Math_and_Physics/Chapter14/14.1_CircularMotion/Example_14.1_AverageAngularVelocityOfCD-ROM/AverageAngularVelocityOfCDROM.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AverageAngularVelocityOfCDROM : MonoBehaviour
+{
+    public float rpm;
+    public float rpmRad;
+    public float radius;
+    public float t;
+    public float w;
+    public float initialDisplacement;
+    public float endDisplacement;
+    public float arcStart;
+    public float arcEnd;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      //  initialDisplacement = arcStart / radius;
+      //  endDisplacement = arcEnd / radius;
+      //  w = (endDisplacement - initialDisplacement) / t;
+
+        rpmRad = rpm * radius;
+        w =  rpmRad / t;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
