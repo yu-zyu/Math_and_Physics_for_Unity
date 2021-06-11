d66fd0448573bcd2abee7d869d20589058b6d22e
yu-zyu <siragaigarasi@gmail.com>
2021-06-11 19:16:17 +0900

    ホイール・オブ・フォーチュンをUnityで再現した

---
+++ Assets/Script/Math_and_Physics/Chapter14/14.1_CircularMotion/Example_14.2_AverageAngularAccelerationOfWheels/AverageAngularAccelerationOfWheels.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AverageAngularAccelerationOfWheels : MonoBehaviour
+{
+    public float alpha;
+    public float startT;
+    public float endT;
+    public float startOmega;
+    public float endOmega;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        alpha = (endOmega - startOmega) / (endT - startT);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Script/Math_and_Physics/Chapter14/14.1_CircularMotion/Example_14.3_WheelOfFortune/WheelOfFortune.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WheelOfFortune : MonoBehaviour
+{
+    public float angle;
+    public float omegaI;
+    public float t;
+    public float a;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        angle = (omegaI * t) + (a * Mathf.Pow(t, 2)) / 2;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
