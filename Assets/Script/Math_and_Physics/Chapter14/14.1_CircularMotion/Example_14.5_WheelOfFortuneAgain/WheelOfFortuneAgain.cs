65498fb4412ded4316e05a5dc1f8fae48d5f556e
yu-zyu <siragaigarasi@gmail.com>
2021-06-13 05:18:11 +0900

    再び、ホイールオブフォーチュンをUnityで再現した

---
+++ Assets/Script/Math_and_Physics/Chapter14/14.1_CircularMotion/Example_14.5_WheelOfFortuneAgain/WheelOfFortuneAgain.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WheelOfFortuneAgain : MonoBehaviour
+{
+    public float omegaI;
+    public float omegaF;
+    public float t;
+    public float a;
+    public float at;
+    public float r;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        a = (omegaF - omegaI) / t;
+        at = a * r;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
