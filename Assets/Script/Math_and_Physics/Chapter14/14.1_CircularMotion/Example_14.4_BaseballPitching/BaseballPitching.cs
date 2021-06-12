c4f5337bfe0933bc4172aef62fe334ac1a9e6896
yu-zyu <siragaigarasi@gmail.com>
2021-06-13 02:03:00 +0900

    野球ボールの投球をUnityで再現した

---
+++ Assets/Script/Math_and_Physics/Chapter14/14.1_CircularMotion/Example_14.4_BaseballPitching/BaseballPitching.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseballPitching : MonoBehaviour
+{
+    public float vt;
+    public float omega;
+    public float r;
+    public float t;
+    public float a;
+    public float at;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //vt = omega * r
+        at = a * r;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
