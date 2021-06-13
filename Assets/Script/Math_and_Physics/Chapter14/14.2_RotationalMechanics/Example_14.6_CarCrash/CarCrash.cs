cddfb6b86ea525a40b9239936fd90e4bf3404daa
yu-zyu <siragaigarasi@gmail.com>
2021-06-13 13:27:30 +0900

    カークラッシュをUnityで再現した

---
+++ Assets/Script/Math_and_Physics/Chapter14/14.2_RotationalMechanics/Example_14.6_CarCrash/CarCrash.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarCrash : MonoBehaviour
+{
+    public float r;
+    public float a;
+    public float i;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //r = Ia
+        a = r / i;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
